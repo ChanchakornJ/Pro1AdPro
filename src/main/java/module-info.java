758063229b9075dbd5b73648ead8733394fc80e0
linnlatt <linn.latt.yamone.qz@gmail.com>
2025-09-23 15:09:16 +0700

    try

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module se233.project1 {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.base;
+    requires javafx.fxml;
 
     requires java.desktop;
     requires ffmpeg;
